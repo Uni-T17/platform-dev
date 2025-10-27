1b93bbd077f51380e7e3708205811c8cf315550d
paung29 <aikepaung767@gmail.com>
2025-10-27 23:11:27 +0700

    config fetch method

--- frontend/src/lib/output/response.ts
+++ frontend/src/lib/output/response.ts
@@ -10,3 +10,36 @@ export type VerifyRespone = {
     verifiedToken: string;
     email: string;
 }
+
+export type ConfirmPasswordRespone = {
+     message: string
+     newUserId : number
+}
+
+export type UserProfileRespone = {
+    message : string
+    data : UserProfileDetails
+}
+
+export type UserProfileDetails = {
+    profileCard : ProfileCard
+    creditsBalance : number
+    bookListed : number
+    exchanges : number
+    contactInfo : ContactInfo
+}
+
+export type ProfileCard = {
+    name : string
+    email : string
+    rating : string
+    memberSince : string
+    bio : string
+    liveIn : string
+}
+
+export type ContactInfo = {
+    phone : string
+    address : string
+    prefferedContact : string
+}
