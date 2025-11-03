4ef2b1bb33e7b3eb4fd3d837bba0f9fbbadce1b5
Uni-T17 <mintayzamt17@gmail.com>
2025-11-04 01:23:36 +0700

    Add get book details for user

--- backend/src/routes/user/book.ts
+++ backend/src/routes/user/book.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { userRequestBook } from "../../controllers/request/requestController";
 import {
+  getBookDetails,
   getPublicBooks,
   getUserBooks,
 } from "../../controllers/book/bookController";
@@ -10,5 +11,6 @@ const bookRoutes = Router();
 bookRoutes.post("/request-book", userRequestBook);
 bookRoutes.get("/get-user-books/:ownerId", getUserBooks);
 bookRoutes.get("/get-all-books", getPublicBooks);
+bookRoutes.get("/get-book-details/:bookId", getBookDetails);
 
 export default bookRoutes;
