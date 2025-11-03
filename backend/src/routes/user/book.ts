099b77e9c12af449222514652b19f00c3bf6caa9
Uni-T17 <mintayzamt17@gmail.com>
2025-11-04 01:19:34 +0700

    Done - get public books even after sign in

--- backend/src/routes/user/book.ts
+++ backend/src/routes/user/book.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { userRequestBook } from "../../controllers/request/requestController";
-import { getUserBooks } from "../../controllers/book/bookController";
+import {
+  getPublicBooks,
+  getUserBooks,
+} from "../../controllers/book/bookController";
 
 const bookRoutes = Router();
 
 bookRoutes.post("/request-book", userRequestBook);
 bookRoutes.get("/get-user-books/:ownerId", getUserBooks);
+bookRoutes.get("/get-all-books", getPublicBooks);
 
 export default bookRoutes;
